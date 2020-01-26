143ad77d5ac72ba87aeef44dececadd7fb5ff5d1
Cleyson Lima <cleysonph@gmail.com>
2020-01-26 00:39:43 -0300

    Created the UserProfile model in accounts app

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    photo = models.ImageField(verbose_name='Foto', upload_to='photos')
+    cell_phone = models.CharField('Celular', max_length=16)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Perfil do usuário'
+        verbose_name_plural = 'Perfis dos usuários'
+
+    def __str__(self):
+        return self.user.username
