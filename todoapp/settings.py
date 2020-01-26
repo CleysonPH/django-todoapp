f7321c059826eeb31328cf075c2a28c4ab3fd844
Cleyson Lima <cleysonph@gmail.com>
2020-01-26 00:31:24 -0300

    Created the accounts app

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -51,6 +51,7 @@ THIRD_APPS = []
 PROJECT_APPS = [
     'core.apps.CoreConfig',
     'tasks.apps.TasksConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + PROJECT_APPS
