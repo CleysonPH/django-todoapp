058561d4261c79770732c5f5e3cf2999d7abefd0
Cleyson Lima <cleysonph@gmail.com>
2020-01-26 00:02:50 -0300

    Created the Category model in tasks app

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Nome', max_length=50)
+    description = models.TextField(verbose_name='Descrição', null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Categorias'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
