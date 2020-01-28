96deb22744505f83abb76da577c8e526df958d37
Cleyson Lima <cleysonph@gmail.com>
2020-01-27 22:43:11 -0300

    Show the profile image in navbar

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     photo = models.ImageField(verbose_name='Foto', upload_to='photos')
     cell_phone = models.CharField('Celular', max_length=16)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
 
     class Meta:
         verbose_name = 'Perfil do usuário'
